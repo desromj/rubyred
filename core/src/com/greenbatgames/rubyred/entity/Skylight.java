ea5708a030a017acb4646ae54b6d916e10d44819
Mike <desrochers.m.j@gmail.com>
2016-10-02 11:47:10 -0600

    Add crack and shatter sounds for skylights

--- core/src/com/greenbatgames/rubyred/entity/Skylight.java
+++ core/src/com/greenbatgames/rubyred/entity/Skylight.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.greenbatgames.rubyred.util.Constants;
+import com.greenbatgames.rubyred.util.Utils;
 
 /**
  * Created by Quiv on 01-10-2016.
@@ -29,14 +30,16 @@ public class Skylight extends Platform
     public void act(float delta)
     {
         // No action if we're broken or deactivated
-        if (!active)
+        if (!active || broken)
             return;
 
         lifetime -= delta;
 
         // Only update if we're NOT broken with a positive lifetime
-        if (lifetime < 0.0f)
+        if (lifetime < 0.0f) {
             broken = true;
+            Utils.playSound("sounds/glass-shatter.wav", 1.0f);
+        }
     }
 
 
@@ -69,6 +72,11 @@ public class Skylight extends Platform
 
 
 
-    public void activate() { active = true; }
+    public void activate()
+    {
+        active = true;
+        Utils.playSound("sounds/glass-crack.wav", 1.0f);
+    }
+
     public boolean isBroken() { return broken; }
 }
