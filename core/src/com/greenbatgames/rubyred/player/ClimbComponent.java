763ff17a390b08bf8505fcd91f8120e3c05f9222
Mike <desrochers.m.j@gmail.com>
2016-11-04 11:16:27 -0600

    Finish off logic for ClimbComponent - just need to add in collision handler

--- core/src/com/greenbatgames/rubyred/player/ClimbComponent.java
+++ core/src/com/greenbatgames/rubyred/player/ClimbComponent.java
@@ -1,6 +1,7 @@
 package com.greenbatgames.rubyred.player;
 
 import com.badlogic.gdx.math.Vector2;
+import com.greenbatgames.rubyred.util.Constants;
 
 /**
  * Created by Quiv on 02-11-2016.
@@ -8,9 +9,13 @@ import com.badlogic.gdx.math.Vector2;
 
 public class ClimbComponent extends PlayerComponent
 {
+    /** The point where the player will grab and pivot around while climbing */
     private Vector2 gripPoint;
 
-    boolean climbing;
+    /** The point referring to the bottom corner of the player */
+    private Vector2 basePoint;
+
+    boolean climbing, climbingRight;
 
     public ClimbComponent(Player player) {
         super(player);
@@ -21,6 +26,7 @@ public class ClimbComponent extends PlayerComponent
     @Override
     public void init() {
         gripPoint = new Vector2();
+        basePoint = new Vector2();
         climbing = false;
     }
 
@@ -31,8 +37,26 @@ public class ClimbComponent extends PlayerComponent
 
         if (!climbing) return true;
 
+        // If climbing, we override the speed of the player and move their
+        // basePoint (bottom corner) towards the final gripPoint (grab area)
+        // of the climb. Once the distance between these is negligible, stop climbing
 
+        float newXVel, newYVel;
 
+        // Y factor
+        if (baseY() < gripY())
+            newYVel = Constants.RUBY_CLIMB_SPEED.y;
+        else
+            newYVel = 0f;
+
+        // X factor
+        if (baseX() < gripX())
+            newXVel = Constants.RUBY_CLIMB_SPEED.x;
+        else
+            newXVel = -Constants.RUBY_CLIMB_SPEED.x;
+
+        // Set the new velocity and return false to break any other movement updates
+        player.getBody().setLinearVelocity(newXVel, newYVel);
         return false;
     }
 
@@ -40,15 +64,26 @@ public class ClimbComponent extends PlayerComponent
         Getters and Setters
      */
 
-    public void setGripPoint(float x, float y) {
-        gripPoint.set(x, y);
+    public void startClimbing(Vector2 gripPoint) {
+        this.gripPoint.set(gripPoint.x, gripPoint.y);
+
+        if (gripPoint.x < player.getLeft())
+            climbingRight = false;
+        else
+            climbingRight = true;
+
+        // if we're climbing right, base point is bottom-left of player box
+        // if we're climbing left, base point is bottom-right of player base
+        basePoint.set(
+                (climbingRight) ? player.getLeft() : player.getRight(),
+                player.getBottom()
+        );
+
+        climbing = true;
     }
 
-    public void setGripPoint(Vector2 vector2) {
-        gripPoint.set(vector2.x, vector2.y);
-    }
-
-    public void startClimbing() { climbing = true; }
     public float gripX() { return gripPoint.x; }
     public float gripY() { return gripPoint.y; }
+    public float baseX() { return basePoint.x; }
+    public float baseY() { return basePoint.y; }
 }
