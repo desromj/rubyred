bb54a56d14fcf08cd8323d4e9eb0e69437ed86e3
Mike <desrochers.m.j@gmail.com>
2016-11-12 19:19:55 -0700

    Add animation method to allow for setting an animation partially completed

--- core/src/com/greenbatgames/rubyred/player/AnimationComponent.java
+++ core/src/com/greenbatgames/rubyred/player/AnimationComponent.java
@@ -1,5 +1,6 @@
 package com.greenbatgames.rubyred.player;
 
+import com.esotericsoftware.spine.AnimationState;
 import com.greenbatgames.rubyred.asset.Assets;
 import com.greenbatgames.rubyred.util.Enums;
 
@@ -20,6 +21,18 @@ public class AnimationComponent extends PlayerComponent
 
     @Override
     public boolean update(float delta) {
+        return update(delta, 1f);
+    }
+
+    /**
+     * @param delta
+     * @param percentLeft If the animation changes, this value determines what percentage
+     *                    of the new animation should be played. Passing a value of 0.35 (35%)
+     *                    means that only the last 35% of the new animation will be played.
+     *                    Best used for non-looping animations
+     * @return
+     */
+    public boolean update(float delta, float percentLeft) {
 
         // Set the player position and X orientation
         this.asset.skeleton.setPosition(
@@ -31,6 +44,12 @@ public class AnimationComponent extends PlayerComponent
         if (this.animationState != this.previousState) {
             asset.skeleton.setToSetupPose();
             asset.setAnimation(this.animationState);
+
+            // Set the percentage remaining if it is not 100%
+            if (percentLeft < 1f) {
+                AnimationState.TrackEntry te = asset.animationState.getCurrent(0);
+                te.setTime(te.getAnimation().getDuration() - te.getAnimation().getDuration() * percentLeft);
+            }
         }
 
         this.previousState = this.animationState;
@@ -53,5 +72,11 @@ public class AnimationComponent extends PlayerComponent
     public void setNext(Enums.AnimationState state) {
         this.animationState = state;
     }
+
+    public void setNext(Enums.AnimationState state, float percentLeft) {
+        this.animationState = state;
+
+    }
+
     public String getNextLabel() { return animationState.getLabel(); }
 }
