0f5d3efe7c763451b17105b99ffa2cb193a3ce17
Mike <desrochers.m.j@gmail.com>
2016-11-24 16:31:21 -0700

    Move activateable objects to a common Interface

--- core/src/com/greenbatgames/rubyred/entity/Skylight.java
+++ core/src/com/greenbatgames/rubyred/entity/Skylight.java
@@ -14,7 +14,7 @@ import com.greenbatgames.rubyred.util.Utils;
  * Created by Quiv on 01-10-2016.
  */
 
-public class Skylight extends Platform implements Initializeable
+public class Skylight extends Platform implements Initializeable, Activateable
 {
     private boolean broken, active;
     private float lifetime;
@@ -88,11 +88,13 @@ public class Skylight extends Platform implements Initializeable
 
 
 
+    @Override
     public void activate()
     {
         active = true;
         Utils.playSound("sounds/glass-crack.wav", 1.0f);
     }
 
+    @Override
     public boolean isBroken() { return broken; }
 }
